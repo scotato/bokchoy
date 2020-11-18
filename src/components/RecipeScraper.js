adf881bec7f6ce7cd78b2f411c8b2f5f5fd1b592
Scott Dodge <scott@goldilocks.design>
2020-11-17 22:03:32 -0600

    upgrade cra to v4, add chakra and typescript support, refactor RecipeScraper to hook, refactor Styled Components to Chakra

--- src/components/RecipeScraper.js
+++ src/components/RecipeScraper.js
@@ -1,125 +1,127 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import ReactJson from 'react-json-view'
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Button,
+  Spinner,
+} from "@chakra-ui/react";
+import { LinkIcon } from "@chakra-ui/icons";
+import ReactJson from "react-json-view";
 
-const RecipeScraperSearch = styled.div`
-  display: flex;
-  margin-bottom: 32px;
+const initialState = {
+  isLoading: false,
+  isFailed: false,
+  metadata: null,
+  jsonld: null,
+  message: null,
+  url: "https://www.bonappetit.com/recipe/slow-roast-gochujang-chicken",
+};
 
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
-`
+export const RecipeScraper = () => {
+  const [state, setState] = useState(initialState);
+  const hasData = state.metadata || state.jsonld;
 
-const RecipeScraperInput = styled.input`
-  flex-grow: 1;
-  padding: 16px 32px;
-  border-radius: 64px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  margin-right: 16px;
+  const reset = (message) =>
+    setState({
+      ...initialState,
+      url: state.url,
+      message,
+    });
 
-  @media (max-width: 768px) {
-    margin-bottom: 16px;
-  }
-`
-
-const RecipeScraperButton = styled.button`
-  padding: 16px 32px;
-  background-color: dodgerblue;
-  color: white;
-  border-radius: 64px;
-  border: 0;
-  font-weight: 700;
-  transition: background-color 0.2s ease-out;
-
-  &:disabled {
-    background-color: rgba(0, 0, 0, 0.5);
-  }
-`
-
-class RecipeScraper extends Component {
-  stateInit = {
-    isLoading: false,
-    isFailed: false,
-    metadata: null,
-    jsonld: null,
-    message: null,
-    url: 'https://www.bonappetit.com/recipe/slow-roast-gochujang-chicken'
-  }
-
-  state = this.stateInit
-
-  reset = message => {
-    this.setState({
-      ...this.stateInit,
-      url: this.state.url,
-      message
-    })
-  }
-
-  handleSubmit = (e) => {
-    e && e.preventDefault()
-    const { url } = this.state
-    const isDev = window.location.hostname === 'localhost'
+  const handleSubmit = (e) => {
+    e && e.preventDefault();
+    const { url } = state;
+    const isDev = window.location.hostname === "localhost";
     const lambdaRecipeScraper = isDev
-      ? 'http://localhost:9000/recipe-scraper'
-      : '/.netlify/functions/recipe-scraper'
+      ? "http://localhost:9000/recipe-scraper"
+      : "/.netlify/functions/recipe-scraper";
 
-    this.setState({ isLoading: true });
+    setState({ isLoading: true });
+
     fetch(lambdaRecipeScraper, {
       method: "POST",
       body: JSON.stringify({ url }),
     })
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
+      .then((response) => response.json())
+      .then((json) => {
+        setState({
           isLoading: false,
           isFailed: json.status !== 200,
           metadata: json.metadata,
-          jsonld: json.jsonld
-        })
+          jsonld: json.jsonld,
+        });
       })
-      .catch(err => {
-        console.error(err)
-        this.reset("unable to fetch page")
+      .catch((err) => {
+        console.error(err);
+        reset("unable to fetch page");
       });
-  }
+  };
 
-  componentDidMount = () => {
-    this.handleSubmit()
-  }
+  useEffect(() => {
+    handleSubmit();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  render = () => {
-    const hasData = this.state.metadata || this.state.jsonld
-
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <RecipeScraperSearch>
-          <RecipeScraperInput
+  return (
+    <form onSubmit={handleSubmit}>
+      <HStack>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            color="gray.300"
+            children={
+              state.isLoading ? (
+                <Spinner thickness="4px" size="md" />
+              ) : (
+                <LinkIcon boxSize={5} height="100%" />
+              )
+            }
+          />
+          <Input
             placeholder="url"
-            value={this.state.url}
-            onChange={e => this.setState({ url: e.target.value })}
+            size="lg"
+            value={state.url}
+            onChange={(e) => setState({ url: e.target.value })}
             required
           />
-  
-          <RecipeScraperButton type='submit' disabled={this.state.isLoading}>
-            get data
-          </RecipeScraperButton>
-        </RecipeScraperSearch>
-        
-        {hasData && (
-          <>
-            <h3>metadata</h3>
-            <ReactJson src={this.state.metadata} />
-            <br />
-            <h3>json-ld</h3>
-            <ReactJson src={this.state.jsonld} />
-          </>
-        )}
-        {this.state.message && <p>{this.state.message}</p>}
-      </form>
-    )
-  }
-}
+        </InputGroup>
 
-export default RecipeScraper
+        <Button
+          type="submit"
+          size="lg"
+          colorScheme="blue"
+          disabled={state.isLoading}
+        >
+          get data
+        </Button>
+
+        <Button
+          size="lg"
+          onClick={() =>
+            setState({
+              ...initialState,
+              url: "",
+            })
+          }
+        >
+          clear
+        </Button>
+      </HStack>
+
+      {hasData && (
+        <Box marginTop={9}>
+          <h3>metadata</h3>
+          <ReactJson src={state.metadata} />
+          <br />
+          <h3>json-ld</h3>
+          <ReactJson src={state.jsonld} />
+        </Box>
+      )}
+      {state.message && <p>{state.message}</p>}
+    </form>
+  );
+};
--- src/lambda/recipe-scraper.js
+++ src/lambda/recipe-scraper.js
@@ -1,50 +1,92 @@
-const cheerio = require('cheerio');
-const got = require('got');
+const cheerio = require("cheerio");
+const got = require("got");
 
 function parseJsonLD($) {
-    const result = [];
+  const results = [];
 
-    $('script[type="application/ld+json"]').each(function () {
-        try {
-            result.push(JSON.parse($(this).html()));
-        } catch (err) {
-            console.error(err);
-        }
-    });
-    
-    return result;
+  $('script[type="application/ld+json"]').each(function () {
+    try {
+      results.push(JSON.parse($(this).html()));
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // const types = [
+  //   "Organization",
+  //   "WebSite",
+  //   "WebPage",
+  //   "Article",
+  //   "Recipe",
+  //   "HowToSection",
+  //   "HowToStep",
+  //   "NutritionInformation",
+  //   "Author",
+  //   "Person",
+  //   "ImageObject"
+  // ]
+
+  // results.map(function (result) {
+  //   const context = result["@context"];
+  //   const type = result["@type"];
+  //   const graph = result["@graph"];
+  //   const isSchema = context.includes("schema.org");
+
+  //   if (isSchema) {
+  //     if (graph) {
+  //       let graphDict = graph.reduce(function (acc, cur) {
+  //         let id = cur["@id"];
+
+  //         if (id) {
+  //           acc[id] = cur;
+  //         } else {
+  //           console.log("no id", cur);
+  //         }
+
+  //         return acc;
+  //       }, {});
+
+  //       console.log(graph);
+  //       console.log(graphDict);
+  //     } else if (type) {
+  //     }
+  //   }
+  // });
+
+  return results;
 }
 
 function parseMetadata($) {
   const result = {};
-  
-  $('meta').each(function () {
-      const $tag = $(this);
-      const name = $tag.attr('name') || $tag.attr('property');
-      if (name) {
-          result[name] = $tag.attr('content');
-      }
+
+  $("meta").each(function () {
+    const $tag = $(this);
+    const name = $tag.attr("name") || $tag.attr("property");
+    if (name) {
+      result[name] = $tag.attr("content");
+    }
   });
 
-  $('head title').each(function () {
-      result.title = $(this).text();
+  $("head title").each(function () {
+    result.title = $(this).text();
   });
 
   return result;
 }
 
 const headers = {
-  "Access-Control-Allow-Origin" : "*",
-  "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-}
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept",
+};
 
-exports.handler = async event => {
+exports.handler = async (event = {}) => {
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
-  
+
   const { url } = JSON.parse(event.body);
-  
+
   try {
     const { body: html } = await got(url);
     const $ = cheerio.load(html);
@@ -56,18 +98,18 @@ exports.handler = async event => {
       body: JSON.stringify({
         status: 200,
         metadata,
-        jsonld
+        jsonld,
       }),
-      headers
-    }
+      headers,
+    };
   } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({
-          status: 500,
-          message: error.response.body
-        }),
-        headers
-      }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        status: 500,
+        message: error.response.body,
+      }),
+      headers,
+    };
   }
-}
+};
