ddd4960f2d69e8f6739707093f1bc8950bb178a8
Scott Dodge <scott@goldilocks.design>
2020-11-23 22:41:22 -0600

    add websites from cache to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,19 @@
-import * as React from "react"
-import { MainLayout } from "../layouts/MainLayout"
+import * as React from 'react'
+import { MainLayout } from '../layouts/MainLayout'
+import { useWebsites } from '../hooks/use-websites'
+import { Box, Text } from '@chakra-ui/react'
 
-export const Home = () => (
-  <MainLayout />
-)
+export const Home = () => {
+  const { websites } = useWebsites()
+  return (
+    <MainLayout>
+      {websites.map((website) => (
+        <Box key={website.id}>
+          <Text>{website.id}</Text>
+          <Text>{website.url}</Text>
+          <Text>{website?.graph?.recipe?.name}</Text>
+        </Box>
+      ))}
+    </MainLayout>
+  )
+}
