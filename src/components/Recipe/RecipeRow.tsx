6912e96e06903b77323d6562b88e9e6e3f0a18d3
Scott Dodge <scott@goldilocks.design>
2020-11-30 22:41:47 -0600

    add authors and organizations to recipe row

--- src/components/Recipe/RecipeRow.tsx
+++ src/components/Recipe/RecipeRow.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import { VStack, HStack, Text, Image, Skeleton } from '@chakra-ui/react'
 import { Recipe as SchemaRecipe, WebPage } from 'schema-dts'
-
 interface RecipeRowProps {
   recipe: SchemaRecipe
   webpage?: WebPage
+}
+
+type Person = {
+  name: string
+}
+
+type Organization = {
+  name: string
 }
 
 export const RecipeRow = (props: RecipeRowProps) => {
@@ -13,6 +20,19 @@ export const RecipeRow = (props: RecipeRowProps) => {
   const thumbnailUrl = recipe.thumbnailUrl as string
   const imageUrl = Array.isArray(recipe.image) ? recipe.image[0] : recipe.image
   const image = imageUrl || thumbnailUrl
+  const author = Array.isArray(recipe?.author)
+    ? (recipe?.author as Person[])
+    : (recipe?.author as Person) ?? []
+  const publisher = Array.isArray(recipe?.publisher)
+    ? (recipe?.publisher as Organization[])
+    : (recipe?.publisher as Organization) ?? []
+  const authors = Array.isArray(author)
+    ? author.map((a) => a.name)
+    : [author.name]
+  const publishers = Array.isArray(publisher)
+    ? publisher.map((a) => a.name)
+    : [publisher.name]
+  const detail = [...authors, ...publishers].join(', ')
 
   return (
     <HStack p={0} spacing={4}>
@@ -28,8 +48,14 @@ export const RecipeRow = (props: RecipeRowProps) => {
         borderRadius={12}
       />
 
-      <VStack spacing={2} alignItems="start">
+      <VStack spacing={1} alignItems="start">
         <Text fontSize={20} lineHeight="1.2" children={title} />
+        <Text
+          fontSize={16}
+          lineHeight="1.2"
+          color="gray.500"
+          children={detail}
+        />
       </VStack>
     </HStack>
   )
