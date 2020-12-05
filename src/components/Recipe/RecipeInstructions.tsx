76df9383621523ee825dadbe24e5acba2d5f418b
Scott Dodge <scott@goldilocks.design>
2020-12-05 11:44:02 -0600

    add support for how to sections on recipe instructions

--- src/components/Recipe/RecipeInstructions.tsx
+++ src/components/Recipe/RecipeInstructions.tsx
@@ -1,30 +1,48 @@
 import * as React from 'react'
-import { HowToStep } from 'schema-dts'
+import { HowToStep, HowToSection } from 'schema-dts'
 import { Box, Heading, Text, OrderedList, ListItem } from '@chakra-ui/react'
 import { useTemplate } from '../../hooks'
 
 interface RecipeInstructionProps {
-  instructions: HowToStep[]
+  instructions: HowToStep[] | HowToSection[]
 }
+
+interface InstructionProps {
+  items: HowToStep[]
+}
+
+const Instructions = (props: InstructionProps) =>
+  Array.isArray(props.items) ? (
+    <OrderedList>
+      {props.items.map((instruction, i) => (
+        <ListItem mb={4} children={instruction.text ?? instruction} key={i} />
+      ))}
+    </OrderedList>
+  ) : (
+    <Text children={props.items} />
+  )
 
 export const RecipeInstructions = (props: RecipeInstructionProps) => {
   const { isLarge } = useTemplate()
+  const sections = props.instructions as HowToSection[]
+  const instructions = props.instructions as HowToStep[]
+  const hasSections =
+    Array.isArray(sections) && sections.filter((s) => s?.itemListElement).length
 
   return (
     <Box px={isLarge ? 12 : 6} paddingTop={4} paddingBottom={8}>
       <Heading fontWeight="bold" fontSize={24} children="Instructions" />
-      {Array.isArray(props.instructions) ? (
-        <OrderedList>
-          {props.instructions.map((instruction, i) => (
-            <ListItem
-              mb={4}
-              children={instruction.text ?? instruction}
-              key={i}
-            />
-          ))}
-        </OrderedList>
+      {hasSections ? (
+        sections.map((section, i) => (
+          <Box key={i}>
+            <Heading fontSize={16} color="gray.500">
+              {section.name}
+            </Heading>
+            <Instructions items={section.itemListElement as HowToStep[]} />
+          </Box>
+        ))
       ) : (
-        <Text>{props.instructions}</Text>
+        <Instructions items={instructions} />
       )}
     </Box>
   )
